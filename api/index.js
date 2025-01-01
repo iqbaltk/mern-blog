06263e49c1038bf8a2381154e2fa815a3909be46
iqbaltk <iqbaltri156@gmail.com>
2025-01-02 02:20:43 +0700

    koneksi database mongodb

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB:", err);
+  });
 
 const app = express();
 
 app.listen(3030, () => {
-  console.log("Server is running on port http://localhost/3010");
+  console.log("Server is running on port http://localhost:3010");
 });
